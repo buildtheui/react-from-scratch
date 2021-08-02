61878fa5c1bf04a05f31f97601c83670a8647892
Fabián Monsalve <fabian@commure.com>
2021-08-02 10:40:10 -0500

    first setup

---
+++ README.md
@@ -0,0 +1,3 @@
+## Description
+
+A barebones react setup, without using create-react-app just for learning purposes
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactIcon from "./assets/icons/react.svg";
+import "App.css"
+
+const App = () =>  (
+    <>
+        <div>This is a barebones React project</div>
+        <img src={ReactIcon}/>
+    </>)
+
+export default App;
